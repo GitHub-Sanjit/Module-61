9fa49921b70ee4dc6cb65ae60ca8222f5d27e745
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-20 16:57:56 +0600

    Implement Login and Register and Module Summary

--- dragon-news-client/src/pages/Login/Register/Register.js
+++ dragon-news-client/src/pages/Login/Register/Register.js
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { AuthContext } from "../../../context/AuthProvider/AuthProvider";
 
 const Register = () => {
+  const { createUser } = useContext(AuthContext);
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const photoUrl = form.photoUrl.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, photoUrl, email, password);
+
+    createUser(email, password)
+      .then((result) => {
+          const user = result.user;
+          form.reset();
+      })
+      .catch((error) => console.error(error));
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Your Name</Form.Label>
-        <Form.Control
-          name="name"
-          type="text"
-          placeholder="Enter name"
-          required
-        />
+        <Form.Control name="name" type="text" placeholder="Enter name" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPhotoURL">
         <Form.Label>Your Photo URL</Form.Label>
-        <Form.Control
-          name="photoURL"
-          type="url"
-          placeholder="Enter URL"
-          required
-        />
+        <Form.Control name="photoUrl" type="photoURL" placeholder="Enter URL" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
